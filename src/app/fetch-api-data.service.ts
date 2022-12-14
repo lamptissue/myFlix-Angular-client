b71c820d415839fb1ff4cb9c773544fd2cb62c67
lamptissue <ncarribine@hotmail.co.uk>
2022-12-14 09:21:04 +0000

    fixing code

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -9,7 +9,7 @@ const apiUrl = 'https://lamptissue-movie-flix.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegistrationService {
+export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
  // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
@@ -22,7 +22,7 @@ export class UserRegistrationService {
     );
   }
     // Non-typed response extraction
-    private extractResponseData(res: Response): any {
+    private extractResponseData(res: any): any {
       const body = res;
       return body || { };
     }
@@ -85,7 +85,7 @@ getGenre(genreName: string): Observable<any> {
 //Get user
 getUser(userDetails: any): Observable<any> {
   const token = localStorage.getItem('token');
-  return this.http.get(apiUrl + 'users', {headers: new HttpHeaders(
+  return this.http.get(apiUrl + 'users',  {headers: new HttpHeaders(
     {
       Authorization: 'Bearer ' + token,
     })}).pipe(
